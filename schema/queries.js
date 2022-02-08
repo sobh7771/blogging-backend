1a2826c108c9cf94019d5a10785f6530ff08873b
Ahmed Sobh <ahmedsobh@Ahmeds-Air.home>
2022-02-08 17:43:26 +0200

    Implement recommendations query

--- schema/queries.js
+++ schema/queries.js
@@ -1,8 +1,16 @@
-const { GraphQLObjectType, GraphQLInt, GraphQLID } = require("graphql");
+const {
+  GraphQLObjectType,
+  GraphQLInt,
+  GraphQLID,
+  GraphQLNonNull,
+  GraphQLList,
+} = require("graphql");
 const Blog = require("../models/blog");
 const BlogType = require("./types/blog");
 const BlogsEdgeType = require("./types/blogs-edge");
 const UserType = require("./types/user");
+const User = require("../models/user");
+const { isLoggedIn } = require("../utils");
 
 const queries = new GraphQLObjectType({
   name: "Query",
@@ -53,6 +61,24 @@ const queries = new GraphQLObjectType({
       type: UserType,
       resolve(source, args, req, info) {
         return req.user;
+      },
+    },
+    recommendations: {
+      type: new GraphQLNonNull(new GraphQLList(UserType)),
+      async resolve(source, { size = 4 }, req, info) {
+        isLoggedIn(req);
+
+        const { user } = req;
+
+        const users = await User.aggregate([
+          { $match: { _id: { $nin: [...user.following, user._id] } } },
+          { $sample: { size } },
+        ]);
+
+        return users.map((user) => {
+          user = { ...user, id: user._id };
+          return user;
+        });
       },
     },
   },
