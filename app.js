e4b599cf7533da81c73a3b53f9a9bbb726578f65
sobh7771 <sobh7771@gmail.com>
2022-02-09 10:35:38 +0200

    Serve static

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ if (NODE_ENV === "development") {
 
 require("./db/mongoose");
 require("./services/passport");
+const path = require("path");
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const cookie = require("cookie-session");
@@ -13,6 +14,7 @@ const passport = require("passport");
 const schema = require("./schema");
 
 const app = express();
+const public = path.join(__dirname, "./build");
 
 app.use(
   cookie({
@@ -34,5 +36,10 @@ app.use(
     graphiql: true,
   })
 );
+
+app.use(express.static(public));
+app.get("*", (req, res) => {
+  res.sendFile(path(public, "./index.html"));
+});
 
 app.listen(PORT, () => console.log(`Server is up on ${PORT}`));
